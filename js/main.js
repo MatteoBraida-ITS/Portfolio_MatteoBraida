2e79b14da070144352a900eca475df4a8ce5e72a
MatteoBraida-ITS <matteo.braida@allievi.itsdigitalacademy.com>
2026-05-07 00:59:26 +0200

    feat: restyle navbar logo and hamburger to match Figma mockup

--- js/main.js
+++ js/main.js
@@ -6,3 +6,33 @@ bottoneMenu.addEventListener("click", () => {
   const isOpen = navbar.classList.contains("is-open");
   bottoneMenu.setAttribute("aria-expanded", isOpen.toString());
 });
+
+bottoneMenu.addEventListener("pointerdown", () => {
+  gsap.to(bottoneMenu, {
+    x: 3,
+    y: 3,
+    boxShadow: "0px 0px 0 #0d0d0d",
+    duration: 0.1,
+    ease: "power2.out",
+  });
+});
+
+bottoneMenu.addEventListener("pointerup", () => {
+  gsap.to(bottoneMenu, {
+    x: 0,
+    y: 0,
+    boxShadow: "3px 3px 0 #0d0d0d",
+    duration: 0.2,
+    ease: "back.out(2)",
+  });
+});
+
+bottoneMenu.addEventListener("pointerleave", () => {
+  gsap.to(bottoneMenu, {
+    x: 0,
+    y: 0,
+    boxShadow: "3px 3px 0 #0d0d0d",
+    duration: 0.2,
+    ease: "power2.out",
+  });
+});
